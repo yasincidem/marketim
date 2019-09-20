02ab807ef147dade75db9f341b8a8a1dec36a3a0
Yasin <yasincidem@gmail.com>
2019-09-20 03:42:56 +0300

    @Keep added to Order class - preventing getting unreached error

--- app/src/main/java/com/yasincidem/marketim/models/Order.kt
+++ app/src/main/java/com/yasincidem/marketim/models/Order.kt
@@ -1,7 +1,9 @@
 package com.yasincidem.marketim.models
 
+import androidx.annotation.Keep
 import com.squareup.moshi.Json
 
+@Keep
 data class Order (
     @get:Json(name = "date") @Json(name = "date") val date: Int,
     @get:Json(name = "month") @Json(name = "month") val month: Int,
@@ -9,5 +11,6 @@ data class Order (
     @get:Json(name = "orderName") @Json(name = "orderName") val orderName: String,
     @get:Json(name = "productPrice") @Json(name = "productPrice") val productPrice: Float,
     @get:Json(name = "productState") @Json(name = "productState") val productState: String,
-    @get:Json(name = "productDetail") @Json(name = "productDetail") val productDetail: ProductDetail
+    @get:Json(name = "productDetail") @Json(name = "productDetail") val productDetail: ProductDetail,
+    var isExpanded: Boolean = false
 )
