80d7208216a5562be457ab99824e95b7440df7dd
Yasin <yasincidem@gmail.com>
2019-09-22 20:53:39 +0300

    RxJava error logging

--- app/src/main/java/com/yasincidem/marketim/MainActivity.kt
+++ app/src/main/java/com/yasincidem/marketim/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.yasincidem.marketim
 
 import android.os.Bundle
+import android.util.Log
 import com.airbnb.mvrx.BaseMvRxActivity
+import io.reactivex.plugins.RxJavaPlugins
 
 class MainActivity : BaseMvRxActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        RxJavaPlugins.setErrorHandler { e ->
+            Log.w("RXJavaError: ", e.message)
+        }
+
+
     }
 }
